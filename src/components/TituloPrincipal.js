05e6ec6cd06a1b8fe74af641e9950add417156f4
dashdancing <omar.gbet@gmail.com>
2022-04-24 17:04:51 -0500

    import fonts

--- src/components/TituloPrincipal.js
+++ src/components/TituloPrincipal.js
@@ -1,19 +1,19 @@
 import React from "react";
-import front_cover from "../assets/front_cover.png";
+import img_cover from "../assets/img_cover.jpg";
 import orange_cruz from "../assets/orange_cruz.png";
 import "../index.css";
 
 export default function TituloPrincipal() {
  return (
   <div className="background-cover">
+   <img className="img_cover" src={img_cover} alt="imagen de bienestar" />
    <h1 className="title">GUÍA DE PRIMEROS AUXILIOS PSICOLÓGICOS</h1>
-   <h2 className="subtitle">Ayuda a otros cuando más lo necesitan</h2>
-   <img className="front_cover" src={front_cover} alt="front_cover" />
-   <img className="symbol_cruz" src={orange_cruz} alt="symbol_orange_cruz" />
+   <h2 className="subtitle">Para asistir a otros cuando más lo necesitan</h2>
    <p className="textbox">
     Basado en el protocolo A B C D E de Primeros Auxilios Psicológicos de la
     Universidad Católica de Chile en colaboración con el CIGIDEN.
    </p>
+   <img className="symbol_cruz" src={orange_cruz} alt="symbol_orange_cruz" />
   </div>
  );
 }
